06d2c92f1fd8e90a9a0c85eb202a922cc5a4706d
Mohammadshadathusen <shadatrolli05@gmail.com>
2026-02-16 15:48:49 +0530

    Played with forms

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
-from flask import Flask, request,url_for, redirect,session,Response, render_template
-
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-def login():
-    return render_template('login.html')
-@app.route("/submit", methods=["POST"])
-def submit():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    # if username == "admin" and password == "123":
-    #     return render_template('welcome.html', username=username)
-    valid_users = {"admin": "123", "user1": "abc", "user2": "xyz"}
-    if username in valid_users and password == valid_users[username]: 
-        return render_template('welcome.html', username=username)
-    
-    else:        
-        return "Invalid credenrtials. Please try again."
+def home():
+    return render_template("feedback.html")
+
+@app.route('/feedback',methods=["GET", "POST"])
+def feedback():
+    if request.method == "POST":
+        name = request.form.get("username")
+        message = request.form.get("message")
+
+        return render_template("thankyou.html", user=name, message=message)
+    return render_template("feedback.html")
+
